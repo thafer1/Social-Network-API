d9c44677fd9b033e5c0dcaa314033471f184fd77
thafer1 <112597461+ZedZappie@users.noreply.github.com>
2023-02-27 14:47:09 -0500

    Added user model

---
+++ models/user.js
@@ -0,0 +1,34 @@
+// User model
+const user = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trimmed: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Must match a valid email address'],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Thought',
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'User',
+      },
+    ],
+  },
+);
+
+// Virtual that retrieves the length of the user's friends array field on query
+user.virtual('friendCount').get(function () {
+    return friends.length;
+  });
